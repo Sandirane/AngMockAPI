d8da6ae491ccbacd6219545c24c7381d87d598a0
Sandirane <kumarasamy.sandirane.77@gmail.com>
2025-01-24 22:00:23 +0100

    config translaco

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, Injectable, isDevMode, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { I18nService } from '@core/services/i18n.service';
+import { TranslocoLoader, Translation, provideTransloco } from '@ngneat/transloco';
+
+@Injectable({ providedIn: 'root' })
+export class TranslocoModuleLoader implements TranslocoLoader {
+  getTranslation(lang: string): Promise<Translation> {
+    return import(`./core/services/i18n/${lang}.json`).then(m => m.default);
+  }
+}
+
+const i18nService = new I18nService(window);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: [...i18nService.availableLangs],
+        defaultLang: i18nService.lang,
+        prodMode: !isDevMode()
+      },
+      loader: TranslocoModuleLoader
+    }),
+    { provide: LOCALE_ID, useValue: i18nService.lang },
+    { provide: I18nService, useValue: i18nService }]
 };
