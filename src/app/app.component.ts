dcffbe0ab420149851fe184e0b72c1a639fa8b01
Sandirane <kumarasamy.sandirane.77@gmail.com>
2025-01-24 22:30:12 +0100

    update app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '@shared/components/footer/footer.component';
+import { MenuComponent } from '@shared/components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, FooterComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
