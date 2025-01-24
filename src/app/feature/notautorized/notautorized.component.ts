446c2e5189d71c27a0bdd6ee0ccb28dbc8863a47
Sandirane <kumarasamy.sandirane.77@gmail.com>
2025-01-24 21:19:55 +0100

    add translaco and feature

---
+++ src/app/feature/notautorized/notautorized.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notautorized',
+  imports: [],
+  templateUrl: './notautorized.component.html',
+  styleUrl: './notautorized.component.css'
+})
+export class NotautorizedComponent {
+
+}
