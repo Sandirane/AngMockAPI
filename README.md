a64dd1c2e0275fdd6596f105f3d636ed7ebf21ba
Sandirane <kumarasamy.sandirane.77@gmail.com>
2025-01-25 13:59:15 +0100

    add componnent and routing for auth and admin

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AngMockAPI
+
+Angular 19 project using this API RESTFul API: https://mock-api.binaryboxtuts.com/
+
+# Application link for demo
+https://
---
+++ src/app/feature/admin/edit-projet/edit-projet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-projet',
+  imports: [],
+  templateUrl: './edit-projet.component.html',
+  styleUrl: './edit-projet.component.css'
+})
+export class EditProjetComponent {
+
+}
