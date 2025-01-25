a9defb4ee9572f33d74dbbd328eb3049b7e15144
Sandirane <kumarasamy.sandirane.77@gmail.com>
2025-01-25 14:50:54 +0100

    add menu

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { I18nService } from '@core/services/i18n.service';
+import { TranslocoDirective } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule, RouterLink, TranslocoDirective, ReactiveFormsModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+
+  private router = inject(Router);
+  private fb = inject(NonNullableFormBuilder);
+  private i18nService = inject(I18nService);
+
+  availableLangs = this.i18nService.availableLangs;
+  langCtrl = this.fb.control(this.i18nService.lang);
+
+  constructor() {
+    this.langCtrl.valueChanges
+      .pipe(takeUntilDestroyed())
+      .subscribe(lang => this.i18nService.changeLanguage(lang));
+  }
 
 }
